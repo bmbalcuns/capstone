dc9798861ffc5f2561a6c0ed135cf0a313680bc8
Breanna Balcuns <bmbalcuns@live.com>
2020-09-15 15:04:38 -0700

    Permissions

--- Knock_It_Back/api/views.py
+++ Knock_It_Back/api/views.py
@@ -1,22 +1,28 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from django.contrib.auth.models import User
 
 from kib_app.models import Drink
 from .serializers import DrinkSerializer, UserSerializer
+from .permissions import IsUser
 
 class ListDrink(generics.ListCreateAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
+    permission_classes = [IsUser]
 
 class DetailDrink(generics.RetrieveUpdateDestroyAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
+    permission_classes = [IsUser]
 
 class ListUser(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
 
 class DetailUser(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
